5ed9d17ab71d15286af05d455ba3f763521a7aae
Adam <136234521+AdamLikesOwls@users.noreply.github.com>
2025-05-19 22:07:53 +0530

    trail.py

--- gravity.py
+++ gravity.py
@@ -1,17 +1,36 @@
 import pygame 
 
+# Pygame Initialisation
 pygame.init()
 
+# Clock for FPS
 clock = pygame.time.Clock()
 
-width, height = 900,800
+#
+white = (255,255,255)
+# Screen Properties
+width, height = 1200,800
 screen = pygame.display.set_mode((width,height))
 
+# Rectangle Properties
 x,y = 100,100
+rectx,recty = 40,40
 
+# Movement Variables
 gravity = 0.1
-vel = 0 
+acl = 0.02
+speed = 0.5
 
+vel_x = 0
+vel_y = 0 
+
+
+# Platform Classes
+
+def draw(posx,posy,sizex,sizey):
+       platform = pygame.draw.rect(screen,white,[posx,posy,sizex,sizey],30)
+    
+# GameLoop
 running = True
 
 while running:
@@ -23,26 +42,57 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    vel += gravity 
-    y += vel * 0.5
 
-    rect = pygame.draw.rect(screen, (255,255,255),[x,y,40,40], 30)
 
-    key = pygame.KEYDOWN
+    # Bottom screen Collision
+    if y > (height-recty):
+        vel_y = 0
+        y = height-recty
     
-    if key: 
-        vel -= 50
+    if  x > (width-rectx):
+        x = width - rectx
+        vel_x = 0
+
+    if  x < (0):
+        x = 0
+        vel_x = 0
     
-    if y > (height-40):
-        vel = 0
-        y = height-40
+        
 
 
+    # Key Inputs
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        if y == (height-recty):
+            vel_y -= 10
+
+    if keys[pygame.K_a]:
+         vel_x += speed
+         x += vel_x * -acl
 
 
+    if keys[pygame.K_d]:
+        vel_x += speed
+        x += vel_x * acl
+    
+
+    
+    # Gravity
+    vel_y += gravity 
+    y += vel_y * 0.5
+
+
+    # Player and Objects
+    rect = pygame.draw.rect(screen, (255,255,255),[x,y,rectx,recty], 30)
+
+    # Platforms
+    platform1 = draw(600,600,500,40)
+
+    # Screen Update
 
     pygame.display.update()
     pygame.display.flip()
-    clock.tick(120)
+    clock.tick()
 
 pygame.quit()
